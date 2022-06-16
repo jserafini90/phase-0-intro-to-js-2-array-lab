2f7021611dc87f553f23599912090e546adcb1ec
Jessica Serafini <jpitts_kiheihs@yahoo.com>
2022-06-16 17:36:18 -0400

    commit tree 81fdfa127843ac0f951bda47cfd77b1fcbf1e996

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name){
+    cats.push(`${name}`)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(`${name}`)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    console.log(cats)
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const copyCats = [...cats, "Broom"]
+    return copyCats;
+}
+
+
+function prependCat(name){
+    const copyCats = ['Arnold', ...cats]
+    return copyCats;
+}
+
+function removeLastCat(name){
+    const copyCats = cats.slice(0, 2);
+    return copyCats
+}
+
+function removeFirstCat(name){
+    const copyCats = cats.slice(1);
+    return copyCats;
+}
